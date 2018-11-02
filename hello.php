0c9de1053b7464f6f1739e3d8998842888472d4e
Raghav Thakur <raghav@dhcp-206-87-130-69.ubcsecure.wireless.ubc.ca>
2018-11-02 15:21:42 -0700

    Delete php files

--- hello.php
+++
@@ -1,9 +0,0 @@
-<html>
-<p>If PHP is working, you will see "Hello World" below, followed by
-some PHP version information:<hr>
-<?php
-   echo "Hello world";
-   phpinfo();  // Print PHP version and config info
-?>
-
-</html>
