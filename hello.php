42af05326ce99b8a3d948364a47707b90ac8c3b3
Raghav Thakur <raghav@dhcp-206-87-130-69.ubcsecure.wireless.ubc.ca>
2018-11-02 15:09:14 -0700

    Add hello.php

---
+++ hello.php
@@ -0,0 +1,9 @@
+<html>
+<p>If PHP is working, you will see "Hello World" below, followed by
+some PHP version information:<hr>
+<?php
+   echo "Hello world";
+   phpinfo();  // Print PHP version and config info
+?>
+
+</html>
