93bc981b1eca0ef77e0bc8b73ab5faff3ead6e6b
Raghav Thakur <raghav@dhcp-206-87-128-50.ubcsecure.wireless.ubc.ca>
2018-11-13 13:15:08 -0800

    Fixed typo

--- custrep/custrep-delete-rider.php
+++ custrep/custrep-delete-rider.php
@@ -89,19 +89,6 @@
 
 require '../server.php';
 
-function printResult($result)
-{ //prints results from a select statement
-    echo "<br>Got data from table tab1:<br>";
-    echo "<table>";
-    echo "<tr><th>ID</th><th>Name</th><th>Phone</th><th>Address</th></tr>";
-
-    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-        echo "<tr><td>" . $row["NID"] . "</td><td>" . $row["NAME"] . "</td><td>" . $row["PHONE"] . "</td><td>" . $row["ADDRESS"] . "</td></tr>"; //or just use "echo $row[0]"
-    }
-    echo "</table>";
-
-}
-
 // Connect Oracle...
 if ($db_conn) {
 
