9670f079acd9414972678322d6bef4ae59449ab8
Raghav Thakur <raghav@dhcp-206-87-128-50.ubcsecure.wireless.ubc.ca>
2018-11-13 16:18:57 -0800

    Finished custrep-bike-info

---
+++ custrep/custrep-bike-info.php
@@ -0,0 +1,116 @@
+<head>
+    <title>Bike Sharing</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../project.js" type="text/javascript" defer></script>
+    <link rel="stylesheet" href="../project.css">
+</head>
+<div id="wrapper">
+
+    <nav>
+        <div id="mysidenav" class="sidenavigation">
+            <a href="javascript:void(0)" id="closebutton" onclick="closeNav()">&times;</a>
+            <a href="../index.html">Home</a>
+            <a href="../login.html">Login</a>
+            <a id="rider" href="../rider/rider-mainpage.html">&gt; Rider</a>
+            <a id="customerservice" class="active" href="custrep-mainpage.html">&gt; Customer Serv. Rep.</a>
+            <a id="maintenance" href="../technician/technician-mainpage.html">&gt; Maintenance Tech.</a>
+            <a href="../about.html">About</a>
+            <a href="../faq.html">FAQ</a>
+            <a href="../ourbikes.html">Our Bikes</a>
+            <a href="../contact.html">Contact</a>
+        </div>
+
+        <ul id="desktopnavigation">
+            <li><span class="dot"></span><a href="../index.html">Home</a></li>
+            <li><span class="dot"></span><a href="../login.html">Login</a></li>
+            <li class="submenu"><span>&gt; </span><a href="../rider/rider-mainpage.html">Rider</a></li>
+            <li class="submenu active"><span>&gt; </span><a class="active" href="custrep-mainpage.html">Customer
+                    Service</a></li>
+            <li class="submenu"><span>&gt; </span><a href="../technician/technician-mainpage.html">Maintenance Tech.</a>
+            </li>
+            <li><span class="dot"></span><a href="../about.html">About</a></li>
+            <li><span class="dot"></span><a href="../faq.html">FAQ</a></li>
+            <li><span class="dot"></span><a href="../ourbikes.html">Our Bikes</a></li>
+            <li><span class="dot"></span><a href="../contact.html">Contact</a></li>
+        </ul>
+    </nav>
+
+    <div id="rightcol">
+
+        <header>
+            <div id="navicon">
+                <img src="../images/navicon.png" alt="three horizontal bars" width="30" onclick="openNav()">
+            </div>
+
+            <h1>Bike Sharing</h1>
+        </header>
+
+        <div id="shadowbox">
+            <main>
+                <div>
+                    <h3>CUSTOMER SERVICE REP. - Bike Info</h3>
+
+                    <form method="POST" action="custrep-bike-info.php">
+
+                        <input type="submit" value="Get All Bike Info" name="getAllBikeInfo">
+
+                        <p>
+                            Table showing all bikes and their info.
+                        </p>
+
+                    </form>
+                </div>
+            </main>
+
+            <footer>
+                <p>
+                    Copyright &copy; 2018 Bike Sharing <br>
+                    <a href="mailto:email@domain.com">email@domain.com</a>
+                </p>
+            </footer>
+        </div>
+    </div>
+</div>
+
+<?php
+
+require '../server.php';
+
+function printResult($result)
+{ //prints results from a select statement
+    echo "<br>Got data from table tab1:<br>";
+    echo "<table>";
+    echo "<tr><th>ID</th><th>Name</th><th>Phone</th><th>Address</th></tr>";
+
+    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+        echo "<tr><td>" . $row["NID"] . "</td><td>" . $row["NAME"] . "</td><td>" . $row["PHONE"] . "</td><td>" . $row["ADDRESS"] . "</td></tr>"; //or just use "echo $row[0]"
+    }
+    echo "</table>";
+
+}
+
+// Connect Oracle...
+if ($db_conn) {
+
+    if (array_key_exists('getAllBikeInfo', $_POST)) {
+
+        $result = executePlainSQL("SELECT * FROM BIKE");
+        printResult($result);
+    }
+
+    if ($_POST && $success) {
+// POST-REDIRECT-GET -- See http://en.wikipedia.org/wiki/Post/Redirect/Get
+        header("location: custrep-bike-info.php");
+    }
+
+
+// Commit to save changes...
+    OCILogoff($db_conn);
+} else {
+    echo "cannot connect";
+    $e = OCI_Error(); // For OCILogon errors pass no handle
+    echo htmlentities($e['message']);
+}
+
+?>
