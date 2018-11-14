4c979b2e502f49857af787ae4caac991fd04ef81
Raghav Thakur <raghav.thakur.rt@gmail.com>
2018-11-13 23:21:07 -0800

    Commit

--- custrep/custrep-rider-info.php
+++ custrep/custrep-rider-info.php
@@ -91,8 +91,8 @@ require "../server.php";
 function printResult($result)
 {
     echo "<br>Got data from table Bike:<br>";
-    echo "<table style='width: 100%' border='1px solid black'>";
-    echo "<tr><th style='border: 1px solid black'>BIKE_ID</th><th style='border: 1px solid black'>DATE_PURCHASED</th><th style='border: 1px solid black'>LATITUDE</th><th style='border: 1px solid black'>LOGITUDE</th></tr><th style='border: 1px solid black'>IS_BROKEN</th>";
+    echo "<table>";
+    echo "<tr><th>BIKE_ID</th><th>DATE_PURCHASED</th><th>LATITUDE</th><th>LOGITUDE</th></tr><th>IS_BROKEN</th>";
 
     while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
         echo "<tr><td>" . $row["BIKE_ID"] . "</td><td>" . $row["DATE_PURCHASED"] . "</td><td>" . $row["LATITUDE"] . "</td><td>" . $row["LOGITUDE"] . "</td><td>" . $row["IS_BROKEN"] . "</td></tr>"; //or just use "echo $row[0]"
@@ -109,7 +109,7 @@ if ($db_conn) {
     }
 
     if ($_POST && $success) {
-        echo "<h1 style='color: black'>System has been reset!</h1>";
+        echo "<h1 style='color: black'>Showing all riders!</h1>";
     } else if (!$success){
         echo "<h1 style='color: red'>Error!</h1>";
     }
