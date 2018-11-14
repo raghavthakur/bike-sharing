fcbcdc87c712206bb46fd0f496fdf2e972d22a3b
Raghav Thakur <raghav.thakur.rt@gmail.com>
2018-11-13 22:15:57 -0800

    Debugger

--- custrep/custrep-mainpage.php
+++ custrep/custrep-mainpage.php
@@ -93,7 +93,77 @@
 
 <?php
 
-include "../server.php";
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = OCILogon("ora_f4l0b", "a60250157", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+
+function executePlainSQL($cmdstr)
+{
+    // takes a plain (no bound variables) SQL command and executes it
+    // echo "<br>running ".$cmdstr."<br>";
+    global $db_conn, $success;
+    $statement = OCIParse($db_conn, $cmdstr); // There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+    if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn); // For OCIParse errors pass the
+        // connection handle
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+
+    $r = OCIExecute($statement, OCI_DEFAULT);
+    if (!$r) {
+        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+        $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+        echo htmlentities($e['message']);
+        $success = False;
+    } else {
+
+    }
+    return $statement;
+
+}
+
+function executeBoundSQL($cmdstr, $list)
+{
+    /* Sometimes the same statement will be executed for several times ... only
+     the value of variables need to be changed.
+     In this case, you don't need to create the statement several times;
+     using bind variables can make the statement be shared and just parsed once.
+     This is also very useful in protecting against SQL injection.
+     See the sample code below for how this functions is used */
+
+    global $db_conn, $success;
+    $statement = OCIParse($db_conn, $cmdstr);
+
+    if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn);
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+
+    foreach ($list as $tuple) {
+        foreach ($tuple as $bind => $val) {
+            // echo $val;
+            // echo "<br>".$bind."<br>";
+            OCIBindByName($statement, $bind, $val);
+            unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+
+        }
+        $r = OCIExecute($statement, OCI_DEFAULT);
+        if (!$r) {
+            echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+            $e = OCI_Error($statement); // For OCIExecute errors pass the statement handle
+            echo htmlentities($e['message']);
+            echo "<br>";
+            $success = False;
+        }
+    }
+
+}
+
+//include "../server.php";
 //include "../reset-database.php";
 
 if ($db_conn) {
