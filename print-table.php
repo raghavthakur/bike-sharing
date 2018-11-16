62f2da86e1d2d2ffee0b57b70215d767abc76ab5
Raghav Thakur <raghav.thakur.rt@gmail.com>
2018-11-15 23:31:51 -0800

    Renamed create table file

--- print-table.php
+++ print-table.php
@@ -14,10 +14,10 @@ function printTable($resultFromSQL, $namesOfColumnsArray)
     foreach ($namesOfColumnsArray as $name) {
         echo "<th>$name</th>";
     }
-        echo "</tr>";
+    echo "</tr>";
 
 
-    while($row = OCI_Fetch_Array($resultFromSQL, OCI_BOTH)) {
+    while ($row = OCI_Fetch_Array($resultFromSQL, OCI_BOTH)) {
         echo "<tr>";
         $string = "";
         for ($i = 0; $i < sizeof($namesOfColumnsArray); $i++) {
@@ -28,3 +28,31 @@ function printTable($resultFromSQL, $namesOfColumnsArray)
     }
     echo "</table>";
 }
+
+?>
+
+<html>
+<style>
+    table {
+        width: 100%;
+        border: 1px solid black;
+    }
+
+    th {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .7em;
+        background: #666;
+        color: #FFF;
+        padding: 2px 6px;
+        border-collapse: separate;
+        border: 1px solid #000;
+    }
+
+    td {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .7em;
+        border: 1px solid #DDD;
+        color: black;
+    }
+</style>
+</html>
