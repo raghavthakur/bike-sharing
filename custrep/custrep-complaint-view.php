d52fd1690035b711156e512640937e86995afd3a
Raghav Thakur <raghav.thakur.rt@gmail.com>
2018-11-15 20:49:00 -0800

    Fixed typo in bound sql

--- custrep/custrep-complaint-view.php
+++ custrep/custrep-complaint-view.php
@@ -173,7 +173,7 @@ if ($db_conn) {
         );
         executeBoundSQL("SELECT complaint_ID, c.rider_ID, r.name, customer_rep_ID, csr.name, cust_description, agent_notes, urgency_level, complaintDateTime, action_taken, is_resolved
         FROM COMPLAINT c, RIDER r, CUSTOMER_SERVICE_REP csr
-        WHERE c.RIDER_ID = r.RIDER_ID AND c.CUSTOMER_REP_ID = csr.EMPLOYEE_ID", $tuple);
+        WHERE c.RIDER_ID = r.RIDER_ID AND c.CUSTOMER_REP_ID = csr.EMPLOYEE_ID", $alltuples);
         OCICommit($db_conn);
     }
 
