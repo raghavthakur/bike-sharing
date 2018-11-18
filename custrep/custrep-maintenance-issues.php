1d5cd92a71d315c85a5d583bb1336920e2c2df26
Raghav Thakur <raghav.thakur.rt@gmail.com>
2018-11-18 12:35:34 -0800

    Custrep maintain issue testing boundsql

--- custrep/custrep-maintenance-issues.php
+++ custrep/custrep-maintenance-issues.php
@@ -105,18 +105,22 @@ include "../print-table.php";
 if ($db_conn) {
 
     if (array_key_exists('viewIssues', $_POST)) {
-//        $tuple = array(
-//            ":bind1" => $_POST['riderID']
-//        );
-//        $alltuples = array(
-//            $tuple
-//        );
-//        executeBoundSQL("DELETE FROM CUSTREP_RIDER WHERE RIDER_ID=:bind1", $alltuples);
-//        OCICommit($db_conn);
-
-        $result = executePlainSQL("SELECT MI.ISSUEDATETIME,R.RIDER_ID, R.NAME, R.PHONE_NUM, B.BIKE_ID, B.LATITUDE, B.LONGITUDE, MI.ISSUE_DESCRIPTION, MI.TECHNICIAN_NOTES, MI.RESOLVED_DATE, MI.TECHNICIAN_ID, MT.NAME
+        $tuple = array(
+            ":bind1" => $_POST['riderID'],
+            ":bind2" => $_POST['bikeID'],
+            ":bind3" => $_POST['sortBy']
+        );
+        $alltuples = array(
+            $tuple
+        );
+        $result = executeResultBoundSQL("SELECT MI.ISSUEDATETIME,R.RIDER_ID, R.NAME, R.PHONE_NUM, B.BIKE_ID, B.LATITUDE, B.LONGITUDE, MI.ISSUE_DESCRIPTION, MI.TECHNICIAN_NOTES, MI.RESOLVED_DATE, MI.TECHNICIAN_ID, MT.NAME
                                           FROM MAINTENANCE_ISSUE MI, RIDER R , BIKE B, MAINTENANCE_TECHNICIAN MT
-                                          WHERE MI.BIKE_ID = B.BIKE_ID AND MI.RIDER_ID = R.RIDER_ID AND MI.TECHNICIAN_ID = MT.EMPLOYEE_ID");
+                                          WHERE MI.BIKE_ID = B.BIKE_ID AND MI.RIDER_ID = R.RIDER_ID AND MI.TECHNICIAN_ID = MT.EMPLOYEE_ID AND R.RIDER_ID = :bind1", $alltuples);
+        OCICommit($db_conn);
+
+//        $result = executePlainSQL("SELECT MI.ISSUEDATETIME,R.RIDER_ID, R.NAME, R.PHONE_NUM, B.BIKE_ID, B.LATITUDE, B.LONGITUDE, MI.ISSUE_DESCRIPTION, MI.TECHNICIAN_NOTES, MI.RESOLVED_DATE, MI.TECHNICIAN_ID, MT.NAME
+//                                          FROM MAINTENANCE_ISSUE MI, RIDER R , BIKE B, MAINTENANCE_TECHNICIAN MT
+//                                          WHERE MI.BIKE_ID = B.BIKE_ID AND MI.RIDER_ID = R.RIDER_ID AND MI.TECHNICIAN_ID = MT.EMPLOYEE_ID");
 
         $columnNames = array("Date and Time", "Rider ID", "Rider Name", "Phone Number", "Bike ID", "Latitude", "Longitude", "Issue Description", "Technician Notes", "Resolved Date", "Technician ID", "Technician Name");
         printTable($result, $columnNames);
--- server.php
+++ server.php
@@ -24,8 +24,6 @@ function executePlainSQL($cmdstr)
         $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
         echo htmlentities($e['message']);
         $success = False;
-    } else {
-
     }
     return $statement;
 
@@ -67,5 +65,45 @@ function executeBoundSQL($cmdstr, $list)
             $success = False;
         }
     }
+
+}
+
+function executeResultBoundSQL($cmdstr, $list)
+{
+    /* Sometimes the same statement will be executed for several times ... only
+     the value of variables need to be changed.
+     In this case, you don't need to create the statement several times;
+     using bind variables can make the statement be shared and just parsed once.
+     This is also very useful in protecting against SQL injection.
+     See the sample code below for how this functions is used */
+
+    global $db_conn, $success;
+    $statement = OCIParse($db_conn, $cmdstr);
+
+    if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn);
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+
+    foreach ($list as $tuple) {
+        foreach ($tuple as $bind => $val) {
+            // echo $val;
+            // echo "<br>".$bind."<br>";
+            OCIBindByName($statement, $bind, $val);
+            unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+
+        }
+        $r = OCIExecute($statement, OCI_DEFAULT);
+        if (!$r) {
+            echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+            $e = OCI_Error($statement); // For OCIExecute errors pass the statement handle
+            echo htmlentities($e['message']);
+            echo "<br>";
+            $success = False;
+        }
+    }
+    return $statement;
 
 }
